7f5b1111b47f6b5f7499e7c6b3276409372ed6eb
Kalladrob <flamingvpo@gmail.com>
2020-08-03 21:17:41 -0300

    small changes

--- src/components/CharacterCreation/CharacterCreation.js
+++ src/components/CharacterCreation/CharacterCreation.js
@@ -6,7 +6,7 @@ import './CharacterCreation.css'
 class CharacterCreation extends Component {
     
     state = {
-        
+        placeholder: "Enter the Character's Name..."
     }
     
     render = () => {
@@ -17,27 +17,27 @@ class CharacterCreation extends Component {
                     <div className="section">
                         <h1 className="entry-title">NAME</h1>
                         <div className="name-container">  
-                            <input className="character-name" placeholder="Enter the Character's Name..."/>
+                            <input className="character-name" placeholder={this.state.placeholder} onClick={this.setState({placeholder: ''})}/>
                         </div>
                     </div>
                     <div className="section">
                         <h1 className="entry-title">GENDER</h1>
                         <div className="gender-container">
                             <button className="gender-button">Male</button>
-                            <button className="gender-button">Female</button>
+                            <button className="gender-button center-button">Female</button>
                             <button className="gender-button">Other</button>
                         </div>
                     </div>
                     <div className="section">
                         <h1 className="entry-title">RACE</h1>
                         <div className="card-container">
-                            <Card cardName="Human"/>
-                            <Card cardName="Elf"/>
-                            <Card cardName="Dwarf"/>
-                            <Card cardName="Dark Elf"/>
-                            <Card cardName="Halfling"/>
-                            <Card cardName="Orc"/>
-                            <Card cardName="Lich"/>
+                            <Card className="card" cardName="Human"/>
+                            <Card className="card" cardName="Elf"/>
+                            <Card className="card" cardName="Dwarf"/>
+                            <Card className="card" cardName="Dark Elf"/>
+                            <Card className="card" cardName="Halfling"/>
+                            <Card className="card" cardName="Orc"/>
+                            <Card className="card" cardName="Undead"/>
                         </div>
                     </div>
                     <div className="section">

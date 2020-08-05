d3a45bbb8017d8706aaf4ab5b38dd3c1a7d04e1e
Kalladrob <flamingvpo@gmail.com>
2020-08-05 20:38:27 -0300

    index alterations

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,16 +1,39 @@
-import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
-import './style.css'
+import React, { Component } from 'react';
+import Navbar from '../Navbar/Navbar.js'
+import './style.css';
 
-class Menu extends Component{
+class Home extends Component{
 
     render() {
         return (
-            <div className="navbar">
-                <div className="nav-link-container">
-                    <Link className="nav-link" to="/">Home</Link>
-                    <Link className="nav-link" to="/CharacterCreation">Create Character</Link>
-                    <Link className="nav-link" to="/CharacterList">Character List</Link>
+            <div>
+                <Navbar/>
+                <div className="welcome-message"><h1>GREETINGS, ADVENTURERS!</h1></div>
+                <div className="intro-text">
+                    <text>
+                        This is a tool to help you create and/or embark in grand adventures!
+                    </text>
+                    <text>
+                        Here, you'll be able to create and manage characters, send them in 
+                        adventures created by other players and even create your own for you to play in or share with others.
+                    </text>
+                    <text>
+                        Choose the path forward and godspeed, friends!
+                    </text>
+                </div>
+                <div className="row-div">
+                    <h1 className="row-title">Character</h1>
+                    <div className="row">
+                        <button>Create</button>
+                        <button>Manage</button>
+                    </div>
+                </div>
+                <div className="row-div">
+                    <h1 className="row-title">Adventure</h1>
+                    <div className="row">
+                        <button>Create</button>
+                        <button>Manage</button>
+                    </div>
                 </div>
             </div>
         )
@@ -18,4 +41,4 @@ class Menu extends Component{
 
 }
 
-export default Menu;
+export default Home;
